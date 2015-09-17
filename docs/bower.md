ef9fcbff21a1f83ddfe504e05aa823c2ac8a3614
zodiac <wuxiaolong@meituan.com>
2015-09-17 18:33:55 +0800

    init

---
+++ docs/bower.md
@@ -0,0 +1,22 @@
+###What Is Bower
+Bower是一个客户端技术的软件包管理器，它可用于搜索、安装和卸载如JavaScript、HTML、CSS之类的网络资源。
+
+
+    npm install -g bower
+    
+###bower.json
+    
+    bower init
+    
+###使用bower安装jQuery
+
+    bower install jquery
+    
+    
+###使用bower卸载jQuery    
+    
+    $ bower uninstall jquery
+    
+###Updating packages    
+
+    bower update
--- server/routes/test.js
+++ server/routes/test.js
@@ -2,13 +2,18 @@ import koaRouter from 'koa-router';
 
 
 var test = koaRouter();
+
+var users = [1, 2, 3];
+
 //router
 test
+    .param('user', function *(id, next) {
+        this.user = users[id];
+        if (!this.user) return this.status = 404;
+        yield next;
+    })
     .get('/', function *(next) {
-        this.mongo.db('test').collection('hello').findOne({}, function (err, doc) {
-            console.log(doc);
-        });
-        this.body = yield this.mongo.db("test").collection('hello').findOne();
+        this.body = 'Hello World!';
     })
     .post('/users', function *(next) {
         this.body = 'Hello World!';
@@ -18,6 +23,10 @@ test
     })
     .del('/users/:id', function *(next) {
         this.body = 'Hello World!';
+        this.mongo.db('test').collection('hello').findOne({}, function (err, doc) {
+            console.log(doc);
+        });
+        this.body = yield this.mongo.db("test").collection('hello').findOne();
     });
 
 export
