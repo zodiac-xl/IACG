262a844ff558ee2b92e3944489922fd821c800c4
zodiac <wuxiaolong@meituan.com>
2015-10-12 15:57:50 +0800

    init

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('babel/register');
 
 var config = require('config');
-var server = require('./server');
+var server = require('./server/index.js');
 
 server.listen(config.port, function(){
   console.log('server is running at %s', config.port);
